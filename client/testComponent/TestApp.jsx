17ed1e29dd914d92ccd9e22794cb6c5633a6428f
VAK <kaluginchernyhovsck@mail.ru>
2019-09-16 22:34:15 +0200

    VAK подключил к проекту jsdoc

--- client/testComponent/TestApp.jsx
+++ client/testComponent/TestApp.jsx
@@ -1,7 +1,21 @@
+/**
+ * тестовый компонент
+ * @author VAK
+ * @module TestApp
+ * @since 0.0.1
+ */
 import './TestApp.scss';
 import React from 'react';
 import koticImg from '~/assets/images/testType1/test.jpg';
 import koticComponentImg from './test_component.jpg';
+
+/**
+ * @function getRandom генерирует рандомно 0 или 1
+ * @return {number} рандом 0 или 1
+ */
+function getRandom() {
+    return getRound(Math.random());
+}
 
 function TestApp() {
     return (
@@ -9,6 +23,7 @@ function TestApp() {
             <p className="test">Test page!!!</p>
             <img src={koticImg}></img>
             <img src={koticComponentImg}></img>
+            <p className="test">Random number: {getRandom()}</p>
         </>
     );
 }
